89dc304dafbbf8aa9fe122cafb39dd55fe03d310
marceloderas675 <marceloderas675@gmail.com>
2025-11-21 17:13:35 -0500

    alu stuff

---
+++ alu_32b.v
@@ -0,0 +1,14 @@
+//alu to perform arithmetic and binary operations
+
+`include "Single Cycle Processor/adder_32b"
+
+module alu_32b 
+
+(
+    input logic [31:0] in0, 
+    input logic [31:0] in1,
+    output logic [31:0] out
+
+);
+
+endmodule
