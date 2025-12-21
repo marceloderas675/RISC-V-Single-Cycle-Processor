b306b3c340cb80b092277881531a3fe423404a45
marceloderas675 <marceloderas675@gmail.com>
2025-12-21 03:34:40 -0500

    changes to regfile and working on a datapath

--- register_file_32b.v
+++ register_file_32b.v
@@ -4,12 +4,27 @@ module register_file_32b
 (
     input  logic clk,
     input  logic wen,
-    input  logic waddr,
-    input  logic wdata,
-    input  logic raddr0,
-    output logic rdata0,
-    input  logic raddr1,
-    output logic rdata1
+    input  logic [4:0] waddr,
+    input  logic [31:0] wdata,
+    input  logic [4:0] raddr0,
+    output logic [31:0] rdata0,
+    input  logic [4:0] raddr1,
+    output logic [31:0] rdata1
 );
+
+    logic [31:0] regfile [32];
+
+    always_ff @(posedge clk) begin
+
+        if (wen) begin
+            regfile[waddr] <= wdata;
+        end
+    end
+
+    always_comb begin
+        regfile[5'b00000] = 32'b0;
+        rdata0 = regfile[raddr0];
+        rdata1 = regfile[raddr1];
+    end
 
 endmodule
