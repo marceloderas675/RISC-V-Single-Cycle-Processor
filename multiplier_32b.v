fafdbd12c7bcf70046d06664bde8ba4a6e881c18
marceloderas675 <marceloderas675@gmail.com>
2025-12-24 00:57:40 -0500

    going to try to make in something that isn't vscode

--- multiplier_32b.v
+++ multiplier_32b.v
@@ -1,12 +1,12 @@
 //multiplier
 
-module Multiplier 
+module multiplier 
 (
     input  logic [31:0] in0,
     input  logic [31:0] in1,
     output logic [31:0] prod
 );
 
-prod = in0 * in1;
+assign prod = in0 * in1;
 
 endmodule
